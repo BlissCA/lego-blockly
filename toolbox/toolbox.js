63e3e2d59bea9f8d875649e41999810d93e83edb
YvesLev <yveslev@gmail.com>
2026-03-10 20:44:17 -0400

    Add "NOT" block in Logic Category

--- toolbox/toolbox.js
+++ toolbox/toolbox.js
@@ -9,6 +9,18 @@ const toolbox = {
         { "kind": "block", "type": "controls_if" },
         { "kind": "block", "type": "logic_compare" },
         { "kind": "block", "type": "logic_operation" },
+            {
+              "kind": "block",
+              "type": "logic_negate",
+              "inputs": {
+                "BOOL": {
+                  "shadow": {
+                    "type": "logic_boolean",
+                    "fields": { "BOOL": "TRUE" }
+                  }
+                }
+              }
+            },
         { "kind": "block", "type": "logic_boolean" },
         { "kind": "block", "type": "logic_null" },
         { "kind": "block", "type": "logic_ternary" }
