d2ee4353d2562b976c805491fe69d52560068748
YvesLev <yveslev@gmail.com>
2026-03-15 20:27:36 -0400

    Corrections

--- toolbox/toolbox.js
+++ toolbox/toolbox.js
@@ -278,6 +278,7 @@ const toolbox = {
           "kind": "category",
           "name": "Outputs Single Port",
           "contents": [
+            { "kind": "block", "type": "Legob_outportalpha" },
             {
               "kind": "block",
               "type": "lego_out_on",
