6fab3d10417beff40c9b30a8b1616ddd8b9ec391
YvesLev <yveslev@gmail.com>
2026-03-29 10:55:34 -0400

    Debug Error when Updating

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // FINAL AUTO-UPDATE SERVICE WORKER
 // ------------------------------
 
-const CACHE_NAME = "lego-blockly-cache-v2026-03-29-1039"; // bump on each deploy
+const CACHE_NAME = "lego-blockly-cache-v2026-03-29-1054"; // bump on each deploy
 
 const ASSETS = [
   "./",
