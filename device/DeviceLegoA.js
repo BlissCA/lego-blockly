0f58b4fdbac70c93e73bfd3516ce709ec34efaf0
YvesLev <yveslev@gmail.com>
2026-04-03 01:18:59 -0400

    Add Lego Interface A

--- device/DeviceLegoA.js
+++ device/DeviceLegoA.js
@@ -78,6 +78,7 @@ export class LegoInterfaceA {
     } catch (err) {
       this._logStatus("Error during connect: " + err);
       await this._safeClose();
+      throw err;
       return false;
     }
   }
