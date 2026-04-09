7079ac69913e7e93fed1973f0ef6b4a6b3a2e685
YvesLev <yveslev@gmail.com>
2026-04-09 17:38:19 -0400

    Add an interactive display block

--- blocks/lego_blocks.js
+++ blocks/lego_blocks.js
@@ -2028,7 +2028,7 @@ Blockly.Blocks['display_value'] = {
     
     this.setPreviousStatement(true);
     this.setNextStatement(true);
-    this.setColour(160);
+    this.setColour("#1a1a1a");
   }
 };
 
