9e26c595b412de972d1f9b0e325b1eb0b993dbda
YvesLev <yveslev@gmail.com>
2026-04-11 16:28:52 -0400

    Add WeDo 1.0 (USB Hub)

--- device/deviceManager.js
+++ device/deviceManager.js
@@ -3,6 +3,7 @@
 import { LegoInterfaceA } from './DeviceLegoA.js';
 import { LegoInterfaceB } from './DeviceLegoB.js';
 import { LegoRcx } from './DeviceLegoRcx.js';
+import { LegoWeDo1 } from './DeviceLegoWeDo1.js';
 
 export class DeviceManager {
   constructor() {
