28e1603827d331139bf2c4ebbc532a7f3809cff2
YvesLev <yveslev@gmail.com>
2026-04-14 09:32:25 -0400

    LegoB Add block Out with Cmd DropBox

--- generators/lego_generators.js
+++ generators/lego_generators.js
@@ -83,6 +83,7 @@ javascriptGenerator.forBlock["lego_out"] = function (block) {
 `;
 };
 
+
 function legoCmd(block, method) {
   const dev  = block.getFieldValue("DEVICE");
   const port = javascriptGenerator.valueToCode(block, "PORT", javascriptGenerator.ORDER_NONE) || "0";
@@ -101,6 +102,10 @@ javascriptGenerator.forBlock["lego_out_on"]    = b => legoCmd(b, "outOn");
 javascriptGenerator.forBlock["lego_out_onl"]   = b => legoCmd(b, "outOnL");
 javascriptGenerator.forBlock["lego_out_onr"]   = b => legoCmd(b, "outOnR");
 javascriptGenerator.forBlock["lego_out_off"]   = b => legoCmd(b, "outOff");
+javascriptGenerator.forBlock["lego_out_float"] = b => legoCmd(b, "outFloat");
+javascriptGenerator.forBlock["lego_out_rev"]   = b => legoCmd(b, "outRev");
+javascriptGenerator.forBlock["lego_out_l"]     = b => legoCmd(b, "outL");
+javascriptGenerator.forBlock["lego_out_r"]     = b => legoCmd(b, "outR");
 
 javascriptGenerator.forBlock["lego_out_offall"] = function (block) {
   const dev  = block.getFieldValue("DEVICE");
@@ -114,11 +119,6 @@ javascriptGenerator.forBlock["lego_out_offall"] = function (block) {
 }
 `;
 };
-
-javascriptGenerator.forBlock["lego_out_float"] = b => legoCmd(b, "outFloat");
-javascriptGenerator.forBlock["lego_out_rev"]   = b => legoCmd(b, "outRev");
-javascriptGenerator.forBlock["lego_out_l"]     = b => legoCmd(b, "outL");
-javascriptGenerator.forBlock["lego_out_r"]     = b => legoCmd(b, "outR");
 
 javascriptGenerator.forBlock["lego_out_pow"] = function (block) {
   const dev  = block.getFieldValue("DEVICE");
