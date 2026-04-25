1638634911e5c6fffaea8b5ded1f7860810dab50
YvesLev <yveslev@gmail.com>
2026-04-25 19:11:01 -0400

    Add Lego Int.A V2

--- device/DeviceLegoA_v2.js
+++ device/DeviceLegoA_v2.js
@@ -110,7 +110,7 @@ export class LegoInterfaceA_v2 {
     }
 
     await this.port.open({
-      baudRate: 115200,
+      baudRate: 19200,
       dataBits: 8,
       stopBits: 1,
       parity: "none"
