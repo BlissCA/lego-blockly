6671772d2b295b82680a25f336c685b25a7db858
YvesLev <yveslev@gmail.com>
2026-05-10 19:56:41 -0400

    Add LPF2 Support

--- device/DeviceLegoLPF2.js
+++ device/DeviceLegoLPF2.js
@@ -10,6 +10,7 @@ const SUBCMD_START_SPEED_FOR_DEGREES = 0x0B;
 const SUBCMD_GOTO_ABS_POS     = 0x0D;
 const SUBCMD_GOTO_REL_POS     = 0x0A;
 const SUBCMD_STOP = 0x0A;
+const SUBCMD_START_SPEED_FOR_TIME = 0x09;
 
 // Brake modes
 const BRAKE_FLOAT = 0x00;
