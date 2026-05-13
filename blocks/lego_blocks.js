b8cdd29baa0a6b7499992504cd70a73b591dfd21
YvesLev <yveslev@gmail.com>
2026-05-13 16:49:30 -0400

    Add LPF2 Support

--- blocks/lego_blocks.js
+++ blocks/lego_blocks.js
@@ -2020,7 +2020,7 @@ window.addEventListener("load", () => {
         {
           "type": "input_value",
           "name": "PORT",
-          "check": "Number",
+          "check": ["Number", "String"]
         }
       ],
       "inputsInline": true,
@@ -2040,7 +2040,7 @@ window.addEventListener("load", () => {
         {
           "type": "input_value",
           "name": "PORT",
-          "check": "Number"
+          "check": ["Number", "String"]
         },
         {
           "type": "input_value",
@@ -2892,7 +2892,7 @@ Blockly.Blocks['vll_unitms'] = {
 Blockly.Blocks['lpf2_ports'] = {
   init: function() {
     this.appendDummyInput()
-      .appendField(new Blockly.FieldDropdown([["A","A"],["B","B"],["C","C"],["D","D"],["F","F"],["F","F"],["AB","AB"],["CD","CD"],["TILT","TILT"]]), "LPF2PORTS");
+      .appendField(new Blockly.FieldDropdown([["A","A"],["B","B"],["C","C"],["D","D"],["E","E"],["F","F"],["AB","AB"],["CD","CD"],["TILT","TILT"]]), "LPF2PORTS");
 
     this.setOutput(true, "String");
     this.setColour(80);
--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 // version.js
-const LEGO_BLOCKLY_VERSION = "2026-05-13-1622";
+const LEGO_BLOCKLY_VERSION = "2026-05-13-1649";
