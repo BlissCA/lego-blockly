8f381e0e613d7104f7b76a92e18074a1eb130ae4
YvesLev <yveslev@gmail.com>
2026-07-10 00:27:32 -0400

    Add Bitwise operation blocks

--- toolbox/toolbox.js
+++ toolbox/toolbox.js
@@ -60,7 +60,7 @@ const toolbox = {
     {
       "kind": "category",
       "name": "Bitwise",
-      "colour": "#aeaeae",
+      "colour": "#999999",
       "contents": [
         { "kind": "block", "type": "bitwise_operation" },
         { "kind": "block", "type": "bitwise_not" },
--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 // version.js
-const LEGO_BLOCKLY_VERSION = "2026-07-10-0006";
+const LEGO_BLOCKLY_VERSION = "2026-07-10-0027";
